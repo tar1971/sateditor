743b23cc88064b0150667187118944235be9d72a
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-11 03:08:47 +0200

    Add files via upload

---
+++ installer5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+######################################################################################
+## Command=wget https://raw.githubusercontent.com/tar1971/sateditor/main/installer.sh -O - | /bin/sh
+##
+###########################################
+###########################################
+
+#!/bin/sh
+ # 
+ # # 
+cd /tmp
+set -e 
+wget "https://raw.githubusercontent.com/tar1971/sateditor/main/ts-sateditor_3.3.tar.gz"
+wait
+tar -xzf ts-sateditor_3.3.tar.gz  -C /
+wait
+cd ..
+set +e
+rm -f /tmp/ts-sateditor_3.3.tar.gz
+echo "   UPLOADED BY  >>>>   TAR_TAR "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
