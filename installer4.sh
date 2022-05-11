21c83e4617d57b85584621ff371a8cdbd993b0b0
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-11 03:03:39 +0200

    Update installer4.sh

--- installer4.sh
+++ installer4.sh
@@ -3,7 +3,7 @@
  # # 
 cd /tmp
 set -e 
-wget "https://raw.githubusercontent.com/tar1971/EPG-PRO/main/EPG-PRO-grabber.tar.gz"
+wget "https://raw.githubusercontent.com/tar1971/sateditor/main/EPG-PRO-grabber.tar.gz"
 wait
 tar -xzf EPG-PRO-grabber.tar.gz  -C /
 wait
@@ -17,6 +17,7 @@ echo "**************************************************************************
 wait
 killall -9 enigma2
 exit 0
+
 
 
 
