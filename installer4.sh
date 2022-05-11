142b6e7a4a398aa22ccff607d17dd7d729ccdf49
tar1971 <96209005+tar1971@users.noreply.github.com>
2022-05-11 03:01:05 +0200

    Add files via upload

---
+++ installer4.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+ # 
+ # # 
+cd /tmp
+set -e 
+wget "https://raw.githubusercontent.com/tar1971/EPG-PRO/main/EPG-PRO-grabber.tar.gz"
+wait
+tar -xzf EPG-PRO-grabber.tar.gz  -C /
+wait
+cd ..
+set +e
+rm -f /tmp/EPG-PRO-grabber.tar.gz
+echo "   UPLOADED BY  >>>>   TAR_TAR "   
+sleep 4;                                                                                                                  
+echo ". >>>>         RESTARING     <<<<"
+echo "**********************************************************************************"
+wait
+killall -9 enigma2
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
